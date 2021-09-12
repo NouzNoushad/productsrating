6e76f210979a9936d1a67f497a598ec1d522078e
NouzNoushad <77887277+NouzNoushad@users.noreply.github.com>
2021-09-12 15:34:42 +0530

    modified cookie

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const session = require('express-session');
+const session = require('cookie-session');
 const passport = require('passport')
 const flash = require('connect-flash');
 const dotenv = require('dotenv');
